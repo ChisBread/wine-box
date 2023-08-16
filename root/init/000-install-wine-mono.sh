b2dd613e38e63b062f4d12f0d65316250a56efbf
sureer <507811581@qq.com>
2023-08-16 11:22:56 +0000

    🚀 release: 升级wine到8.0.2

--- root/init/000-install-wine-mono.sh
+++ root/init/000-install-wine-mono.sh
@@ -19,10 +19,10 @@ function skipsomething() {
         sleep 0.2
     done
 }
-wget https://dl.winehq.org/wine/wine-mono/7.0.0/wine-mono-7.0.0-x86.msi
-wine msiexec /i wine-mono-7.0.0-x86.msi &
-sleep 10
+wget http://mirrors.ustc.edu.cn/wine/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
+wine msiexec /i wine-mono-8.0.0-x86.msi &
+sleep 20
 skipsomething "wine"
 wait
 sleep 5
-rm wine-mono-7.0.0-x86.msi
+rm wine-mono-8.0.0-x86.msi
